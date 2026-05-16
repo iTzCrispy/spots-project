efea60f27fb997bbed07b6eb943ce3ff0e57c7a7
Chris <chpalacios25@gmail.com>
2026-05-16 02:37:33 -0700

    corrections needed

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ const editProfileDescriptionInput = editProfileModal.querySelector(
 );
 
 editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal__is-opened");
+  editProfileModal.classList.add(".modal_is-opened ");
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
@@ -23,8 +23,8 @@ const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const newPostForm = newPostModal.querySelector(".modal__form");
-const newImageinput = newPostModal.querySelector("#card-image-input");
-const newCaptioninput = newPostModal.querySelector("#card-caption-input");
+const newImageInput = newPostModal.querySelector("#card-image-input");
+const newCaptionInput = newPostModal.querySelector("#card-caption-input");
 
 newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal__is-opened");
@@ -48,13 +48,17 @@ function handleEditProfileSubmit(evt) {
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
-const newImageElement = document.querySelector(".card-image-input");
-const newCaptionElement = document.querySelector(".card-caption-input");
+const newImageElement = document.querySelector("#card-image-input");
+const newCaptionElement = document.querySelector("#card-caption-input");
 
 function handleNewPostSubmit(evt) {
   evt.preventDefault();
-  newImageinput = editProfileNameInput.value;
-  newCaptioninput = editProfileDescriptionInput.value;
+  const imageUrl = newImageInput.value;
+  const caption = newCaptionInput.value;
+
+  console.log(newImageInput);
+  console.log(newCaptionInput);
+
   newPostModal.classList.remove("modal__is-opened");
 }
 
